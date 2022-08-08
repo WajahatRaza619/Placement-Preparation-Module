31d36146d9a9b34632089c68747a92f2fe75c98e
Wajahat Raza <73235940+WajahatRaza619@users.noreply.github.com>
2022-08-08 18:18:31 +0530

    Create Making Anagrams.java

---
+++ Strings/Making Anagrams.java
@@ -0,0 +1,18 @@
+public static int makingAnagrams(String s1, String s2) {
+       int[] chars = new int[26];
+    for (char c1 : s1.toCharArray()) { 
+        chars[c1-97]++; 
+        }
+    for (char c2 : s2.toCharArray()) {
+         chars[c2-97]--; 
+         }
+    int count = 0;
+    for(int i: chars){
+        count += Math.abs(i);
+        }
+
+      return count;
+
+    }
+
+}
